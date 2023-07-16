684d57f965a1c7fafafea0e5a4788c385557c25b
sidneyboy <100180350+sidneyboy@users.noreply.github.com>
2023-07-16 14:15:02 +0800

    updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,9 +45,18 @@
                             <div class="alert alert-success border-left-success alert-dismissible fade show"
                                 role="alert">
                                 {{ session('success') }}
-                                {{-- <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
-                                </button> --}}
+                                </button>
+                            </div>
+                        @endif
+                        @if (session('error'))
+                            <div class="alert alert-danger border-left-danger alert-dismissible fade show"
+                                role="alert">
+                                {{ session('error') }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
                             </div>
                         @endif
                     </div>
